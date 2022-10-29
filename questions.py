7152ac7d6786fccd4fd3d492d9722848112e34f7
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2022-10-29 18:48:28 +0100

    draft question structure

---
+++ questions.py
@@ -0,0 +1,52 @@
+        self.questions = [
+            {
+            'id' : 1, 
+            'question' : 'Select an option:'
+            'metric_skill' : []
+            'metric_bcg' : [] 
+            'option 1' : None,
+            'option 2' : None,
+            'option 3' : None,
+            'option 4' : None,
+            },
+            {
+            'id' : 2, 
+            'question' : 'Select an option:'
+            'metric_skill' : []
+            'metric_bcg' : [] 
+            'option 1' : None,
+            'option 2' : None,
+            'option 3' : None,
+            'option 4' : None,
+            },
+            {
+            'id' : 3, 
+            'question' : 'Select an option:'
+            'metric_skill' : []
+            'metric_bcg' : [] 
+            'option 1' : None,
+            'option 2' : None,
+            'option 3' : None,
+            'option 4' : None,
+            },
+            {
+            'id' : 4, 
+            'question' : 'Select an option:'
+            'metric_skill' : []
+            'metric_bcg' : [] 
+            'option 1' : None,
+            'option 2' : None,
+            'option 3' : None,
+            'option 4' : None,
+            },
+            {
+            'id' : 5, 
+            'question' : 'Select an option:'
+            'metric_skill' : []
+            'metric_bcg' : [] 
+            'option 1' : None,
+            'option 2' : None,
+            'option 3' : None,
+            'option 4' : None,
+            },
+        ]
