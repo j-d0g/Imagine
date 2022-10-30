4328a37f904f845b006b3245af3edd8627fc868f
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2022-10-30 11:35:15 +0000

    role-match algorithm complete

--- user.py
+++ user.py
@@ -1,55 +1,64 @@
+from collections import namedtuple
 class User:
-    skill_translations = {
-            'prob_solving' : 0
-            'communication' : 1
-            'teamwork' : 2
-            'creativity' : 3 
-            'leadership' : 4
-            'artistic' : 5
-            'academic' : 6 
-            'attention_to_detail' : 7
-            'prioritisation' : 8
-            'interpersonal' : 9 
-            'time_management' : 10
-    }
-
-    bcg_translations = {
-            'minority' : 0,
-            'gender' : 1,
-            'financial' : 2,
-            'location' : 3,
-            'education' : 4,
-            'disability' : 5,
-            'domestic' : 6,
-            'lgbt' : 7
-    }
-
-    def __init__(self, name, skill_score, bcg_score, ):
-        self.name = 
-        self.id = 
+    def __init__(self, name, email, skill_score, bcg_score):
+        # roles[0] = Software Engineer, roles[1] = UX/UI Design, roles[2] = Product Manager
+        self.roles = [
+                        {
+                                'prob_solving' : 5,
+                                'communication' : 3,
+                                'teamwork' : 3,
+                                'creativity' : 1, 
+                                'leadership' : 1,
+                                'artistic' : 0,
+                                'academic' : 3, 
+                                'prioritisation' : 3,
+                                'interpersonal' : 1,
+                                'handle_pressure' : 3
+                        },
+                        {
+                                'prob_solving' : 2,
+                                'communication' : 2,
+                                'teamwork' : 2,
+                                'creativity' : 4, 
+                                'leadership' : 1,
+                                'artistic' : 3,
+                                'academic' : 1,
+                                'prioritisation' : 2,
+                                'interpersonal' : 2,
+                                'handle_pressure' : 2
+                        },
+                        {
+                                'prob_solving' : 2,
+                                'communication' : 3,
+                                'teamwork' : 2,
+                                'creativity' : 1, 
+                                'leadership' : 3,
+                                'artistic' : 1,
+                                'academic' : 1, 
+                                'prioritisation' : 3,
+                                'interpersonal' : 3,
+                                'handle_pressure' : 3
+                        }
+                    ]
+        self.name = name
+        self.id = email
         self.skill_score = skill_score
         self.bcg_score = bcg_score
 
-class Googler(User):
-    def __init__():
+    def calculate_best_match(self):
+        best_match = {
+            'role_id' : -1,
+            'score_diff' : 99
+        }
+        for role_id, role_score in enumerate(self.roles):
+            diff = 0
+            print(f'role: {role_id}')
+            for attribute in role_score.keys():
+                diff += abs(self.skill_score[attribute] - role_score[attribute])
+                print(f'attribute: {attribute}\nstudent score: {self.skill_score[attribute]},\nrole score: {role_score[attribute]},\ndiff: {diff}')
+            if diff < best_match['score_diff']:
+                best_match['role_id'], best_match['score_diff'] = role_id, diff
+        return best_match
 
-class Student(User):
-    def __init__(self):
-        self.best_match_abs = [] * self.get_total_googlers
-        self.best_match_career = [] * self.get_total_googlers
-        self.best_match_bcg = [] * self.get_total_googlers
-    
-    def get_total_googlers(self):
-        return 5
-
-    def get_best_skill(self):
-        return self.best_match_skill[0]
-        
-    def get_best_bcg(self):
-        return self.best_match_bcg[0]
-        
-    def get_best_abs(self):
-        return self.best_match_abs[0]
-
-new_user = User()
-print(new_user.skill_score['prob_solving'])
+student_1 = User('Jordan', 'jordan_tran1@hotmail.com', user_score, None)
+print(student_1.calculate_best_match())
